57e565e94ddd176ad9bbfeb85cd1b16592014086
kavyanpv <22ituos135@ddu.ac.in>
2024-08-10 17:31:52 +0530

    Added flutter

---
+++ lib/login.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Mylogin extends StatefulWidget {
+  const Mylogin({super.key});
+
+  @override
+  State<Mylogin> createState() => _MyloginState();
+}
+
+class _MyloginState extends State<Mylogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/login.png'), fit: BoxFit.cover)),
+      child: const Scaffold(
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+}
